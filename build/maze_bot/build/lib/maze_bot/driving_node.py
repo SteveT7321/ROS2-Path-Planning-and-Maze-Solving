e3ba40d18a434221812fe40863063f910d4c68bc
SteveT <82607704+SteveT7321@users.noreply.github.com>
2024-04-19 01:04:50 +0800

    Add files via upload

---
+++ build/maze_bot/build/lib/maze_bot/driving_node.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('velocity_publisher')
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        timer_period = 0.5  
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x=1.5
+        #msg.angular.z=0.5
+        
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
